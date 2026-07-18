61658235318449803c7063df1a9ad17861611cb5
JueSung <peter@paikman.com>
2026-07-17 20:30:29 -0500

    jump and landing animations prob other stuff and EXPORT

--- player.gd
+++ player.gd
@@ -7,6 +7,8 @@ const HORIZONTAL_SPEED: = 400.
 const HORIZONTAL_AIR_FRICTION := 4000. # factor in which slows down horizontal velocity while in air
 const JUMP_SPEED := 750.
 
+const MAX_VERTICAL_SPEED = 1500
+
 
 const DASH_SPEED := 800.
 const DASH_TIME := .18 # seconds (time length of dash)
@@ -32,10 +34,16 @@ const WALL_JUMP_FRICTION_TIME: float = .5 # time higher friction is applied
 var wall_jump_friction_timer:= 0.0
 
 
-var last_tick_on_floor: bool = true # on_floor from last tick - used for coyote time
+var last_tick_on_floor: bool = true # on_floor from last tick - used for coyote time and landing animation
 var COYOTE_TIME := .05 # you get this many seconds to after falling off edge or dash ending to jump still
 var COYOTE_TIME_POST_DASH := 0.1 # for after dashing, need a bit more maybe keep TODO
 var coyote_timer := 0.
+
+var prev_dir := Vector2i(0,0) # for animation stuff
+var prev_velocity := Vector2(0,0)
+var land_finished := true # for moving from animation "land" to "idle" or "run"
+const LAND_SPEED_THRESHOLD := 800. # vertical speed cutoff to run "land" animation
+
 
 enum INPUTS {
 	up,
@@ -67,7 +75,6 @@ func _process(_delta):
 			inputs[input] = false
 	
 
-
 func _physics_process(delta: float) -> void:
 	if dash_pause_timer >= 0.0:
 		dash_pause_timer -= delta
@@ -83,7 +90,7 @@ func _physics_process(delta: float) -> void:
 			coyote_timer = COYOTE_TIME_POST_DASH
 			dash_cooldown_timer = DASH_COOLDOWN
 
-			set_animation("idle", Vector2.ZERO)
+			set_animation("idle" if is_on_floor() else "jump", Vector2.ZERO)
 
 	if coyote_timer > 0:
 		coyote_timer -= delta
@@ -107,8 +114,10 @@ func _physics_process(delta: float) -> void:
 		if abs(velocity.x) > 0:
 			set_animation("run", dir)
 		else:
-			set_animation("idle", dir)
+			if $AnimatedSprite2D.animation != "idle":
+				set_animation("idle", dir)
 
+	# WALL CLUTCHING------------------------------------------------------------------------------------------------------------------------------
 	# var friction_factor := 1. # default 1 aka no friction
 	if is_on_wall():
 			
@@ -128,15 +137,15 @@ func _physics_process(delta: float) -> void:
 		else:
 			wall_clutching = false
 			if $AnimatedSprite2D.animation == "wall_clutch":
-				set_animation("idle", dir)
+				set_animation("idle" if is_on_floor() else "jump", dir)
 	else:
 		wall_clutching = false
 		if $AnimatedSprite2D.animation == "wall_clutch":
-			set_animation("idle", dir)
+			set_animation("idle" if is_on_floor() else "jump", dir)
 		
 	
 	
-
+	# Applying vertical gravity -----------------------------------------------------------------------------------------------------------------
 	if !is_on_floor():
 		if !dashing && dash_pause_timer <= 0: # don't apply gravity if dashing
 			if velocity.y < 0: # going up
@@ -164,13 +173,16 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, dir.x * HORIZONTAL_SPEED, alpha * delta)
 		
 		if last_tick_on_floor && !has_jumped: # just left floor and wasn't by jumping
+			set_animation("jump", dir)
 			coyote_timer = COYOTE_TIME
 		
 	else:
 		if dir.x == 0 && inputs["down"]: # not moving and want to crouch
 			# set hitbox diff and sprite diff TODO
 			set_animation("crouch", dir)
-
+		
+		if !last_tick_on_floor && abs(prev_velocity.y) > LAND_SPEED_THRESHOLD: # just landed
+			set_animation("land", dir)
 
 		if has_jumped:
 			has_jumped = false
@@ -179,7 +191,7 @@ func _physics_process(delta: float) -> void:
 		if wall_jump_friction_timer > 0.0:
 			wall_jump_friction_timer = 0.0
 	
-
+	# JUMP BLOCK-----------------------------------------------------------------------------------------------------------------------------
 	# can jump out of (and cancel) a dash
 	if inputs["up"] && (((!has_jumped && (is_on_floor())) || ((dashing || wall_clutching || coyote_timer > 0) && has_released_up))):
 		#why_jump(has_jumped, is_on_floor(), dashing, wall_clutching, has_released_up, coyote_timer)
@@ -189,7 +201,7 @@ func _physics_process(delta: float) -> void:
 		has_released_up = false
 		velocity.y = -1. * JUMP_SPEED
 
-		set_animation("idle", dir) # TODO change to some jump one?
+		set_animation("jump", dir) # TODO change to some jump one?
 
 		if dashing:
 			dashing = false
@@ -209,7 +221,7 @@ func _physics_process(delta: float) -> void:
 		has_released_dash = true
 
 
-	# checking for dash
+	# DASH BLOCK--------------------------------------------------------------------------------------------------------------------------------------
 	if inputs["dash"] && !dashing && !has_dashed && !wall_clutching && has_released_dash && dash_cooldown_timer <= 0.0:
 		
 		# basicaly overwrite all velocity for dashing in one of the 8 directions
@@ -234,11 +246,13 @@ func _physics_process(delta: float) -> void:
 	last_tick_on_floor = is_on_floor()
 
 	# set terminal velocity for falling y # TODO maybe keep idk
-	if velocity.y > 1300:
-		velocity.y = 1300
+	if velocity.y > MAX_VERTICAL_SPEED:
+		velocity.y = MAX_VERTICAL_SPEED
 
 	# # TODO idk if like maybe get rid of
-	# match $AnimatedSprite2D.animation:
+	if $AnimatedSprite2D.animation == "jump":
+		$AnimatedSprite2D.frame = int(abs(velocity.y)**2. / (MAX_VERTICAL_SPEED**2.) * 4)
+		# $AnimatedSprite2D.scale = Vector2(0.6 * (1-abs(velocity.y/6000.)), 0.6 * (1+abs(velocity.y / 6000.)))
 	# 	"idle":
 	# 		$AnimatedSprite2D.scale = Vector2(0.6 * (1 - abs(velocity.y/6000.)), 0.6 * (1+abs(velocity.y / 6000.)))
 	# 	"run":
@@ -248,7 +262,10 @@ func _physics_process(delta: float) -> void:
 	# 	"wall_clutch":
 	# 		$AnimatedSprite2D.scale = Vector2(0.1 * (1 - abs(velocity.y/6000.)), 0.1 * (1+abs(velocity.y / 6000.)))
 
-				
+	if !dashing && !dash_pause_timer > 0 && dir.x != prev_dir.x:
+		set_animation("", dir)
+	prev_dir = dir
+	prev_velocity = velocity
 	move_and_slide()
 
 
@@ -256,23 +273,38 @@ func _physics_process(delta: float) -> void:
 # dir is vector describing direction looking rn
 func set_animation(animation_name, dir):
 	var animatedSprite2D = $AnimatedSprite2D
+	if animatedSprite2D.animation == animation_name:
+		return
 	animatedSprite2D.rotation = 0
+	if animation_name == "": # prob for setting dir or something
+		animation_name = animatedSprite2D.animation
+
 	match animation_name:
 		"idle":
+			if animatedSprite2D.animation == "land" && !land_finished:
+				return # allow landing animation prior to idle animation
 			animatedSprite2D.animation = "idle"
-			animatedSprite2D.flip_h = false
-			animatedSprite2D.offset = Vector2(-340., -1007)
+			if animatedSprite2D.flip_h == false:
+				animatedSprite2D.offset = Vector2(-340., -1007)
+			else:
+				animatedSprite2D.offset = Vector2(340., -1007)
 			animatedSprite2D.scale = Vector2(0.6, 0.6)
 			$CollisionShape2D.disabled = false
 			$crouch_hitbox.disabled = true
+			animatedSprite2D.stop()
 		"crouch":
 			animatedSprite2D.animation = "crouch"
-			animatedSprite2D.flip_h = false
-			animatedSprite2D.offset = Vector2(-341., -967.)
+			if animatedSprite2D.flip_h == false:
+				animatedSprite2D.offset = Vector2(-341., -967.)
+			else:
+				animatedSprite2D.offset = Vector2(341., -967.)
 			animatedSprite2D.scale = Vector2(0.6, 0.6)
 			$CollisionShape2D.disabled = true
 			$crouch_hitbox.disabled = false
+			animatedSprite2D.stop()
 		"run":
+			if animatedSprite2D.animation == "land" && !land_finished:
+				return # allow landing animation prior to run animation
 			animatedSprite2D.animation = "run"
 			if dir.x > 0:
 				animatedSprite2D.flip_h = false
@@ -285,6 +317,8 @@ func set_animation(animation_name, dir):
 
 			$CollisionShape2D.disabled = false
 			$crouch_hitbox.disabled = true
+
+			animatedSprite2D.play()
 		"dash":
 			animatedSprite2D.animation = "dash"
 			animatedSprite2D.flip_h = false
@@ -293,6 +327,8 @@ func set_animation(animation_name, dir):
 			animatedSprite2D.rotation = atan2(dir.y, dir.x) + PI/2.
 			$CollisionShape2D.disabled = false
 			$crouch_hitbox.disabled = true
+
+			animatedSprite2D.play()
 		"wall_clutch":
 			animatedSprite2D.animation = "wall_clutch"
 			if dir.x > 0:
@@ -306,26 +342,51 @@ func set_animation(animation_name, dir):
 
 			$CollisionShape2D.disabled = false
 			$crouch_hitbox.disabled = true
+			animatedSprite2D.stop()
 		"jump":
 			pass
-			# animatedSprite2D.animation = "jump"
-			# animatedSprite2D.offset = Vector2(-646., -799.)
-			# animatedSprite2D.scale = Vector2(0.6, 0.6)
-			# $CollisionShape2D.disabled = false
-			# $crouch_hitbox.disabled = true
+			animatedSprite2D.animation = "jump"
+			var using_dir = dir
+			if using_dir.x == 0:
+				using_dir.x = 1 if animatedSprite2D.flip_h == false else -1
+			if using_dir.x > 0:
+				animatedSprite2D.flip_h = false
+				animatedSprite2D.offset = Vector2(-646., -794.)
+				animatedSprite2D.scale = Vector2(0.6, 0.6)
+			else:
+				animatedSprite2D.flip_h = true
+				animatedSprite2D.offset = Vector2(646., -794.)
+				animatedSprite2D.scale = Vector2(0.6, 0.6)
+			animatedSprite2D.stop()
 		"land":
 			animatedSprite2D.animation = "land"
-			animatedSprite2D.flip_h = false
-			animatedSprite2D.offset = Vector2(-724., -775.)
+			var using_dir = dir
+			if using_dir.x == 0:
+				using_dir.x = 1 if animatedSprite2D.flip_h == false else -1
+			if using_dir.x > 0:
+				animatedSprite2D.flip_h = false
+				animatedSprite2D.offset = Vector2(-647., -776.)
+			else:
+				animatedSprite2D.flip_h = true
+				animatedSprite2D.offset = Vector2(647., -776.)
 			animatedSprite2D.scale = Vector2(0.6, 0.6)
 			$CollisionShape2D.disabled = false
 			$crouch_hitbox.disabled = true
+			land_finished = false
 
+			animatedSprite2D.play()
 	
-	animatedSprite2D.play()
 
 
-
+func animation_finished() -> void:
+	if $AnimatedSprite2D.animation != "land":
+		return
+	land_finished = true
+	if prev_dir.x != 0:
+		set_animation("idle", prev_dir)
+	else:
+		set_animation("run", prev_dir)
+	
 
 # for testing
 # (((!has_jumped && (is_on_floor())) || ((dashing || wall_clutching || coyote_timer > 0) && has_released_up)))
